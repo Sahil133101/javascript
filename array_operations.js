94b260aaf4a488026f986e9bdbebedc16bbac84f
sahil133101 <sahilsharmanakur@gmail.com>
2023-04-04 21:06:46 +0530

    array operations

---
+++ array_operations.js
@@ -0,0 +1,24 @@
+let a = ["hii i am sahil"]
+a.push("sharma")
+console.log(a)
+
+let b = [10,20,30,40]
+b.pop()
+console.log(b)
+
+let c =[10,20,40,50]
+c.shift()
+console.log(c)
+
+let d = [212509]
+d.unshift(11)
+console.log(d)
+
+var myArray = [1, "Hello", [2, 3]];
+console.log(myArray.slice(1, 2));
+
+var Array = [1, "Hello", [2, 3]];
+Array.splice(1, 1, "Hello World", [4, 5]);
+console.log(Array)
+
+
