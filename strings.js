80d3da9d4d14e951f2bdb13d272108a6e5da80ea
sahil133101 <sahilsharmanakur@gmail.com>
2023-04-04 16:05:27 +0530

    string

---
+++ strings.js
@@ -0,0 +1,35 @@
+/*One of the most important aspects of JavaScript is its ability to manipulate strings, which are sequences of characters. In this blog post, we will explore the basics of JavaScript strings and the various string methods that can be used to manipulate them.
+
+A string in JavaScript is a sequence of characters enclosed in either single or double quotes. For example, the following are valid strings in JavaScript*/
+
+
+//length of string 
+
+let  a  = "sahil"
+//console.log(a.length)
+
+//concat
+
+var b = "sahil"
+var c ="sharma"
+console.log(b.concat(c));
+
+//index of 
+
+var str = "Hello World";
+console.log(str.indexOf("W"));
+
+//slice
+
+var str = "Hello World";
+console.log(str.slice(6));
+
+//replace
+
+var str = "Hello World";
+console.log(str.replace("World", "Universe"));
+
+//to upper case and lower case
+var str = "Hello World";
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
