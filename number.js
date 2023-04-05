7d67b23a4a6babd0e4ebb5b1e5faf6234f9a2e1d
sahil133101 <sahilsharmanakur@gmail.com>
2023-04-05 15:34:56 +0530

    number

---
+++ number.js
@@ -0,0 +1,14 @@
+console.log(Number("3.14"));
+// Output: 3.14
+console.log(Number(true));
+// Output: 1
+
+console.log(parseInt("3"));
+// Output: 3
+console.log(parseFloat("3.14"));
+// Output: 3.14
+
+console.log(Number.MAX_VALUE);
+// Output: 1.7976931348623157e+308
+console.log(Number.MIN_VALUE);
+// Output: 5e-324
