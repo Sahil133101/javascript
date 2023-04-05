273cbef6e5876b2673224e0e40c6bc969edc3847
sahil133101 <sahilsharmanakur@gmail.com>
2023-04-05 15:28:04 +0530

    date

---
+++ date.js
@@ -0,0 +1,12 @@
+// let currentDate = new Date();
+// console.log(currentDate);
+
+
+// let date = new Date();
+// date.setDate(22);
+// date.setFullYear(2022);
+// console.log(date);
+
+let date = new Date();
+console.log(date.toLocaleString());
+console.log(date.toLocaleString('en-US', {timeZone: 'UTC'}));
